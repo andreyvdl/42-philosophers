29c456acc3c06abe17270219b19decfbac50476d
adantas- <adantas-@student.42sp.org.br>
2023-06-19 18:03:49 -0300

    +: adding initial files + philosophers.h header with main includes + philosophers.c main of the C + Makefile TODO!!!

---
+++ philo/philosophers.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/19 17:26:04 by adantas-          #+#    #+#             */
+/*   Updated: 2023/06/19 18:02:44 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"./header.h"
+
+/*
+todo: get argv as input - nbr_philos lifetime launch_time amimir [meals]
+todo: learn about pthreads and mutex in C
+todo: read pdf better
+todo: learn basic math to convert time
+?: optimization
+*/
+
+int	main(int argc, char *argv[])
+{
+	
+}
---
+++ philo/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/19 16:59:34 by adantas-          #+#    #+#             */
+/*   Updated: 2023/06/19 17:05:43 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h> // memset
+# include <stdio.h> // printf
+# include <stdlib.h> // malloc, free
+# include <unistd.h> // write, usleep
+# include <sys/time.h> // gettimeofday
+# include <pthread.h> // pthread_create, pthread_detach, pthread_join, pthread_mutex_init, pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
+
+#endif
