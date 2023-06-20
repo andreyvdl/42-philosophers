c937f09e2c99e62928e8f79c03d9222d38633101
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-06-19 22:59:44 -0300

    M: Basic setup M makefile now prints beautiful text and compiles M philosophers.c now compiles a basic main and print text

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,11 +6,11 @@
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 17:26:04 by adantas-          #+#    #+#             */
-/*   Updated: 2023/06/19 18:02:44 by adantas-         ###   ########.fr       */
+/*   Updated: 2023/06/19 22:17:20 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"./header.h"
+#include"./philosophers.h"
 
 /*
 todo: get argv as input - nbr_philos lifetime launch_time amimir [meals]
@@ -22,5 +22,6 @@ todo: learn basic math to convert time
 
 int	main(int argc, char *argv[])
 {
-	
+	printf("tah compilando %d %s\n", argc, *argv);
+	return (0);
 }
